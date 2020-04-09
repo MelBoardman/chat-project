c0d5de9bdf97a1d713a0e30d096bfd46d8ff0fad
MelBoardman <melanie_boardman@hotmail.com>
2020-04-09 13:54:56 +0000

    Added form on Index page

--- run.py
+++ run.py
@@ -1,35 +1,45 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
+
 
 def add_messages(username, message):
     """Add messages to the message list"""
     now = datetime.now().strftime("%H:%M:%S")
     messages.append("({}) {}: {}".format(now, username, message))
 
+
 def get_all_messages():
     """Get all of the messages and separate them using a 'br' tag"""
     return "<br>".join(messages)
 
-@app.route('/')
+
+@app.route('/', methods=["GET", "POST"])
 def index():
     """Min Page with Instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+    if "username" in session:
+        return redirect(session["username"])
+
+    return render_template("index.html")
+
 
 @app.route('/<username>')
 def user(username):
     """Display chat messages"""
     return "<h1>Welcome {0}</h1>{1}" .format(username, get_all_messages())
 
+
 @app.route('/<username>/<message>')
 def send_message(username, message):
     """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
     return redirect("/" + username)
-
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
